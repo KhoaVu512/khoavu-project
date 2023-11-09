e4bb14bb87c6b56eed1a379953db663972f92a30
VincenzoKhoa <khoa.vm.512@gmail.com>
2023-11-09 14:11:56 +0700

    change event

--- src/app/twitter/components/feed/post-item.tsx
+++ src/app/twitter/components/feed/post-item.tsx
@@ -1,33 +1,33 @@
 import Link from 'next/link'
 import { Post } from '@/app/twitter/components/types'
 
-function PostItem(Props: Post) {
+function PostItem(props: Post) {
   return (
     <div>
       <Link
-        href={`/twitter/post/${Props.id}`}
+        href={`/twitter/post/${props.id}`}
         className="hover:bg-gray-900"
       >
         <div className="">
           <div className="w-full flex  mb-1">
             <div className="flex rounded-full mx-4  w-[calc(100% - 4px)] h-[calc(100% - 4px)] mr-4">
               <img
-                src={Props.image}
-                alt={`avatar + ${Props.name}`}
+                src={props.image}
+                alt={`avatar + ${props.name}`}
                 className="w-[42px] h-[40px] rounded-full"
               />
             </div>
             <div className="flex flex-col mr-4 w-[90%]">
               <div className="flex">
-                <div className="mr-2 font-bold">{Props.name}</div>
+                <div className="mr-2 font-bold">{props.name}</div>
                 <div className="mr-1 text-[#71767B]">
-                  {Props.nameId} <span>.</span>
+                  {props.nameId} <span>.</span>
                 </div>
-                <div className="mr-1 text-[#71767B]"> {Props.time}h</div>
+                <div className="mr-1 text-[#71767B]"> {props.time}h</div>
               </div>
-              <div>{Props.content}</div>
+              <div>{props.content}</div>
               <img
-                src={Props.image}
+                src={props.image}
                 alt={'avatar user'}
                 className="w-[95%] max-h-[502px]   mt-6 rounded-2xl"
               />
