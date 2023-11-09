db3775c0cb02756968d412939234e0bd85143f36
VincenzoKhoa <khoa.vm.512@gmail.com>
2023-11-09 12:06:44 +0700

    getData version 2

--- src/app/api/task/add/route.ts
+++ src/app/api/task/add/route.ts
@@ -1,7 +1,7 @@
 import { TaskService } from '../../store'
 
 export async function POST(request: Request) {
-  const { name } = await request.json()
-  const item = TaskService.addTask(name)
+  const { name, nameId, time, content, image, like } = await request.json()
+  const item = TaskService.addTask(name, nameId, time, content, image, like)
   return Response.json({ status: 200, data: item })
 }
